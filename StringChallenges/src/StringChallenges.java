60c28721df1ac6739ee06cecbddfe1b7ca51dce1
andrew.shine <andrew.shine@MU-C1-10.mullen.pvt.k12.co.us>
2018-08-30 08:51:07 -0600

    Halfway through repeat code

--- StringChallenges/src/StringChallenges.java
+++ StringChallenges/src/StringChallenges.java
@@ -3,12 +3,36 @@ public class StringChallenges
 	{
 
 		static Scanner userInput = new Scanner(System.in);
+		static boolean repeat = true;
 		public static void main(String[] args)
 			{
 				// TODO Auto-generated method stub
-				useInsideVoice();
-				printVerticalText();
-				countVowels();
+				Scanner userIntPut = new Scanner(System.in);
+				
+				do
+					{
+						System.out.println("Please pick a challenge you would like to see! \n "
+								+ "1) Use Inside Voice \n"
+								+ "2) Print Vertical text \n"
+								+ "3) Count the Vowels");
+						int userChoice = userIntPut.nextInt();
+						switch (userChoice)
+						{
+							case 1:
+								useInsideVoice();
+								break;
+							case 2:
+								printVerticalText();
+								break;
+							case 3:
+								countVowels();
+								break;
+							default:
+								break;
+						}
+						askToRepeat(repeat);
+					}
+				while(repeat);
 			}
 		public static void useInsideVoice()
 		{
@@ -48,7 +72,21 @@ public class StringChallenges
 							//System.out.println(textToCount.substring(i, i+1));
 						}
 				}
-			System.out.println("There were "+vowelCounter+" vowel(s) in that text.");
+			if(vowelCounter != 1)
+				{
+					System.out.println("There were "+vowelCounter+" vowels in that text.");
+				}
+			else
+				{
+					System.out.println("There was "+vowelCounter+" vowel in that text.");
+				}
+		}
+		public static boolean askToRepeat(boolean repeat)
+		{
+			System.out.println("That was fun! Would you like to play again? (yes/no)");
+			String answer = userInput.nextLine();
+			if (answer.contains)
+			return true;
 		}
 
 	}
